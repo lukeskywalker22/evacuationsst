e069736aae65e6b750c8c629690a0298924aa0d2
luke yeo <87899265+lukeskywalker22@users.noreply.github.com>
2024-03-26 21:59:27 +0800

    Updated name of app :skull:

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:evcuationapp/routepage.dart';
+import 'package:evacuationapp/routepage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
